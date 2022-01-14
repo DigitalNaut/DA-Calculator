ba36ed1b955597b5eb67a3b913954fc88d470f8f
John B <johnbernalfsd@gmail.com>
2022-01-14 17:06:53 -0600

    Fixes to the Subunit component
    - Added a display only tag to toggle its display state & interactivity
    - Now actively stringified its value instead of relying on a hook

--- src/components/Unit/Subunit.tsx
+++ src/components/Unit/Subunit.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEventHandler, useMemo, useState } from 'react';
+import React, { ChangeEventHandler, useState } from 'react';
 
 import { ICompoundInput, ISubunit } from './types';
 
@@ -14,31 +14,35 @@ export function stringifyValue(userInput: string | ICompoundInput) {
   return userInput.join(' ');
 }
 
-export default function Subunit({ index = -1, subunit, input, onChangeInput }: ISubunit) {
+export default function Subunit({
+  index = -1,
+  subunit,
+  input,
+  display,
+  onChangeInput,
+}: ISubunit): JSX.Element | null {
   const [userInput, setInput] = useState(input);
 
-  const stringifiedValue = useMemo(() => stringifyValue(userInput), [userInput]);
+  if (!userInput[1]?.length && userInput[0] === 1) return null;
 
-  if (!userInput[1]?.length && userInput[0] === 1) return <></>;
-
-  if (typeof userInput === 'string')
+  if (display)
     return (
       <input
         disabled
-        className="p-2 text-center text-green-900 bg-green-100 w-min"
-        value={userInput}
+        className="p-2 text-center text-green-900 bg-green-100"
+        value={stringifyValue(input)}
       />
     );
 
   const onChange: ChangeEventHandler<HTMLInputElement> = (e) =>
     setInput(makeCompoundValue(e.currentTarget.value));
 
-  const submitInput = () => onChangeInput(stringifyValue(userInput), index, subunit);
+  const submitInput = () => onChangeInput?.(stringifyValue(userInput), index, subunit);
 
   return (
     <input
       className="w-full p-2 text-center bg-green-700"
-      value={stringifiedValue}
+      value={stringifyValue(userInput)}
       onChange={onChange}
       onBlur={submitInput}
     />
