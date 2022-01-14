28611927f52b6ccec0e1ff80a433cd0402244b08
John B <johnbernalfsd@gmail.com>
2022-01-13 23:28:54 -0600

    Added dummy data and 3 examples
    Added 3 different expressions showcasing labels being cancelled out when necessary

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -3,41 +3,54 @@ import React from 'react';
 import Expression from 'src/components/Expression';
 import { IExpression } from 'src/components/types';
 
-type ExpressionProps = {
-  input: IExpression;
-};
-
-export function Expression({ input }: ExpressionProps) {
-  return (
-    <div className="flex items-center justify-center w-full p-3">
-      {input.map((aUnit) => (
-        <Unit key={aUnit.toString()} input={aUnit} />
-      ))}
-      <button type="button">
-        <FontAwesomeIcon icon={faEquals} />
-      </button>
-      <div>Result</div>
-    </div>
-  );
-}
-
-const expression: IExpression = [
+const expression1: IExpression = [
   [
     [2, 'grapes'],
+    [1, 'm'],
+  ],
+  [
+    [60, 'm'],
     [1, 'h'],
   ],
   [
     [24, 'h'],
     [1, 'd'],
   ],
-  [[5, 'd']],
+  [[5, 'd'], [1]],
+];
+const expression2: IExpression = [
+  [[15, 'wood']],
+  [
+    [4, 'planks'],
+    [1, 'wood'],
+  ],
+  [
+    [1, 'slabs'],
+    [3, 'planks'],
+  ],
+];
+const expression3: IExpression = [
+  [
+    [2, 'grapes'],
+    [1, 'm'],
+  ],
+  [
+    [60, 'm'],
+    [1, 'h'],
+  ],
+  [
+    [24, 'h'],
+    [1, 'd'],
+  ],
 ];
 
 export default function Home(): JSX.Element {
   return (
     <div className="w-screen h-screen bg-gray-900">
       <div className="flex flex-col items-center w-full">
-        <Expression input={expression} />
+        <Expression input={expression1} />
+        <Expression input={expression2} />
+        <Expression input={expression3} />
       </div>
     </div>
   );
