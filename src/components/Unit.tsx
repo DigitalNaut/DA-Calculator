a9f07ccfdc0ac0c27b8d852770693a8524fdc4bc
John B <johnbernalfsd@gmail.com>
2022-01-13 23:06:47 -0600

    Reworked the Unit component
    - Added a Label sub-component to handle String inputs
    - Implemented style switch for when the input is just a string

--- src/components/Unit.tsx
+++ src/components/Unit.tsx
@@ -6,13 +6,28 @@ export function Separator() {
 }
 
 export function Subunit({ input }: ISubunit) {
+  if (typeof input === 'string')
+    return (
+      <input disabled className="w-min p-2 bg-green-100 text-green-900 text-center" value={input} />
+    );
+
+  if (!input[1]?.length && input[0] === 1) return <></>;
+
   return (
     <input
       disabled
       className="w-full p-2 bg-green-700 text-center"
-      value={`${input[0]} ${input[1]}`}
+      value={input[1]?.length ? `${input[0]} ${input[1]}` : input[0]}
     />
   );
+}
+
+type LabelProps = {
+  input: string;
+};
+
+export function Label({ input }: LabelProps) {
+  return <input disabled className="w-full p-2 bg-green-700 text-center" value={input} />;
 }
 
 export default function Unit({ input }: IUnit) {
