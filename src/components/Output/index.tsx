843298f466f084132a1fe3389eb9d26e6ecbba86
John B <johnbernalfsd@gmail.com>
2022-01-15 19:59:33 -0600

    Fixed up the output component
    Changed its CSS properties to a more subtle dark bg

--- src/components/Output/index.tsx
+++ src/components/Output/index.tsx
@@ -6,6 +6,6 @@ type Props = {
 
 export default function Output({ children }: Props) {
   return (
-    <input disabled className="p-2 m-1.5 text-center text-blue-900 bg-blue-50" value={children} />
+    <input disabled className="p-2 m-1.5 text-center text-white bg-gray-800" value={children} />
   );
 }
