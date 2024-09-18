f14c0118570ca49c3546801e00aa3a852bab27b3
John Bernal <johnbernalfsd@gmail.com>
2024-09-18 17:12:08 -0600

    Simplify app structure

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,7 @@ import ExpressionsProvider from "./hooks/expressions-context/ExpressionsProvider
 export default function App(): JSX.Element {
   return (
     <ExpressionsProvider>
-      <div className="bg-gray-800 font-sans text-white">
-        <Home />
-      </div>
+      <Home />
     </ExpressionsProvider>
   );
 }
