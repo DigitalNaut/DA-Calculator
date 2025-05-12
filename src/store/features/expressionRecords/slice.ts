a3169d76dc343fe584fa903ee5185683fd34e6bb
John Bernal <johnbernalfsd@gmail.com>
2025-05-11 22:42:27 -0600

    Organize store expression presets

--- src/store/features/expressionRecords/slice.ts
+++ src/store/features/expressionRecords/slice.ts
@@ -5,50 +5,8 @@ import { createExpression } from "src/logic/expressions";
 import type { AppThunk } from "src/store";
 import type { Expression } from "src/types/expressions";
 import { randomId } from "src/utils/id";
+import { expression1 } from "./presets";
 import type { ExpressionRecord, ExpressionRecords } from "./types";
-
-const expression1 = createExpression([
-  {
-    numerator: { factor: 2, labels: { grapes: 1 } },
-    denominator: { factor: 1, labels: { m: 1 } },
-  },
-  {
-    numerator: { factor: 60, labels: { m: 1 } },
-    denominator: { factor: 1, labels: { h: 1 } },
-  },
-  {
-    numerator: { factor: 24, labels: { h: 1 } },
-    denominator: { factor: 1, labels: { d: 1 } },
-  },
-  {
-    numerator: { factor: 5, labels: { d: 1 } },
-  },
-]);
-// const expression2 = createExpression([
-//   { numerator: { factor: 15, labels: { planks: 1 } } },
-//   {
-//     numerator: { factor: 4, labels: { planks: 1 } },
-//     denominator: { factor: 1, labels: { wood: 1 } },
-//   },
-//   {
-//     numerator: { factor: 1, labels: { wood: 1 } },
-//     denominator: { factor: 3, labels: { planks: 1 } },
-//   },
-// ]);
-// const expression3 = createExpression([
-//   {
-//     numerator: { factor: 2, labels: { grapes: 1 } },
-//     denominator: { factor: 1, labels: { m: 1 } },
-//   },
-//   {
-//     numerator: { factor: 60, labels: { m: 1 } },
-//     denominator: { factor: 1, labels: { h: 1 } },
-//   },
-//   {
-//     numerator: { factor: 24, labels: { h: 1 } },
-//     denominator: { factor: 1, labels: { d: 1 } },
-//   },
-// ]);
 
 /**
  * Adds a random ID and default coordinates to each expression in the array.
