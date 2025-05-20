eb05809ed7c601a3e477129fc5c0273641d8c380
John Bernal <johnbernalfsd@gmail.com>
2025-05-20 04:07:23 -0600

    Refactor expression type derivation

--- src/types/expressions.ts
+++ src/types/expressions.ts
@@ -14,20 +14,24 @@ const QuantitySchema = z.object({
  * The denominator is optional, and if it is not provided, the denominator defaults to 1
  * It includes an id for rendering purposes
  */
-const RatioSchema = z.object({
-  id: z.string().length(18),
+const BaseRatioSchema = z.object({
   numerator: QuantitySchema,
   denominator: QuantitySchema.optional(),
 });
+export const BaseExpressionSchema = z.array(BaseRatioSchema);
 
+export const RatioSchema = BaseRatioSchema.extend({
+  id: z.string().length(18),
+});
 export const ExpressionSchema = z.array(RatioSchema);
-export const BaseExpressionSchema = z.array(RatioSchema.omit({ id: true }));
 
 export type LabelCount = NonNullable<z.infer<typeof QuantitySchema>["labels"]>;
 export type Quantity = z.infer<typeof QuantitySchema>;
 export type Ratio = z.infer<typeof RatioSchema>;
-export type QuantityPosition = keyof Omit<Ratio, "id">;
+//            ^?
+export type BaseRatio = z.infer<typeof BaseRatioSchema>;
+//            ^?
+export type QuantityPosition = keyof BaseRatio;
 export type Expression = z.infer<typeof ExpressionSchema>;
 
 export type BaseExpression = z.infer<typeof BaseExpressionSchema>;
-export type BaseRatio = BaseExpression[0];
